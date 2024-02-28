3c867eef24febae38fcd3a9afa3ebcb8b441be40
tiwarishankar <tiwarishankar587@gmail.com>
2024-02-28 23:47:06 +0530

    add start func desc

--- agents/src/web_retrieve.rs
+++ agents/src/web_retrieve.rs
@@ -1,17 +1,24 @@
 use crate::retrieve_answer;
 use axum::{routing::get, Router};
+use std::net::SocketAddr;
 
-// Function to initialize and start the server
-pub async fn start() -> anyhow::Result<()> {
-    // Create a new Axum router
+/// Starts the Axum server for retrieval.
+///
+/// This function initializes the Axum router, defines routes, and binds the server to a specified
+/// address. It then starts serving HTTP requests asynchronously.
+///
+/// # Returns
+///
+/// Returns a `Result` containing the `SocketAddr` where the server is listening if the server
+/// starts successfully. If an error occurs during server startup or execution, it returns an
+/// `anyhow::Error`.
 
+pub async fn start() -> anyhow::Result<SocketAddr> {
     println!("coming here in the start function of retrieval!");
     let app = Router::new()
-        .route("/", get(hello_world)) // Home route that returns "Hello, world!"
-        .route("/retrieve", get(retrieve_answer))
-        .route("/answer", get(semantic_retrieve)); // Route to handle 'answer' with `semantic_retrieve` function
+        .route("/", get(hello_world))
+        .route("/retrieve", get(retrieve_answer));
 
-    // Define the address to bind the server to
     let addr = "127.0.0.1:3000".parse().unwrap();
 
     println!("Listening on {}", addr);
@@ -20,22 +27,10 @@ pub async fn start() -> anyhow::Result<()> {
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
-        .unwrap();
-
-    Ok(())
+        .map(|_| addr)
+        .map_err(Into::into)
 }
 
-// Define the `hello_world` function to handle requests to the home route
 async fn hello_world() -> &'static str {
     "Hello, world!"
-}
-
-// Define the `retrieve` function to handle requests to the '/retrieve' route
-// async fn retrieve() -> &'static str {
-//     "Retrieve function called"
-// }
-
-// Define the `semantic_retrieve` function to handle requests to the '/answer' route
-async fn semantic_retrieve() -> &'static str {
-    "Semantic Retrieve function called"
 }
